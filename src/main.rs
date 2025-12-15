e422add7abaf34bbe84065044ca9012c6d4c3198
ridwanalmahmud <ridwanalmahmud107@gmail.com>
2025-12-15 03:37:25 +0000

    feat: implement pwd, cd

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn check_ext_cmd(cmd: &str) -> (bool, Option<PathBuf>) {
 }
 
 fn cmd_type(cmd: &str) {
-    let builtins: [&str; 3] = ["echo", "exit", "type"];
+    let builtins: [&str; 4] = ["echo", "exit", "type", "pwd"];
     if builtins.contains(&cmd) {
         println!("{} is a shell builtin", cmd);
     } else {
@@ -54,6 +54,26 @@ fn exec_ext_cmd(cmd: &str, args: Vec<&str>) {
     print!("{}", String::from_utf8_lossy(&output.stdout).into_owned());
 }
 
+fn cd_cmd(dir: &str) {
+    if dir == "~" {
+        match env::var("HOME") {
+            Ok(val) => {
+                env::set_current_dir(val).expect("Failed to change dir");
+            }
+            Err(e) => {
+                println!("{}", e);
+            }
+        }
+    } else {
+        let path: &Path = Path::new(dir);
+        if path.exists() {
+            env::set_current_dir(dir).expect("Failed to change dir");
+        } else {
+            println!("cd: {}: No such file or directory", dir);
+        }
+    }
+}
+
 fn main() {
     loop {
         print!("$ ");
@@ -63,6 +83,10 @@ fn main() {
         io::stdin().read_line(&mut command).unwrap();
 
         let toks: Vec<&str> = command.trim().split_whitespace().collect();
+        if toks.is_empty() {
+            continue;
+        }
+
         match toks[0] {
             "exit" => break,
             "echo" => {
@@ -75,6 +99,17 @@ fn main() {
             "type" => {
                 if toks.len() > 1 {
                     cmd_type(toks[1]);
+                }
+            }
+            "pwd" => {
+                println!(
+                    "{}",
+                    env::current_dir().expect("Failed to get cwd").display()
+                );
+            }
+            "cd" => {
+                if toks.len() > 1 {
+                    cd_cmd(toks[1]);
                 }
             }
             _ => {
